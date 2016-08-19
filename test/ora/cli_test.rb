5f8a23ef0b315de8f202cfc1909e1857f91a7dcf
Ducksan Cho <ducktyper@gmail.com>
2016-08-20 11:58:16 +1200

    Remove sample test

--- test/ora/cli_test.rb
+++ test/ora/cli_test.rb
@@ -4,8 +4,4 @@ class Ora::CliTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Ora::Cli::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
