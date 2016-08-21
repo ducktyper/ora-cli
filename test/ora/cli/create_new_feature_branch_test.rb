bdb5561e4183fcb337ff6225880974a569bf1edf
Ducksan Cho <ducktyper@gmail.com>
2016-08-21 14:17:01 +1200

    add a.txt

--- test/ora/cli/create_new_feature_branch_test.rb
+++ test/ora/cli/create_new_feature_branch_test.rb
@@ -1,6 +1,48 @@
 require 'test_helper'
 
 class CreateNewFeatureBranchTest < Minitest::Test
+  include Bash
+  def setup
+
+    bash(silent: true) do
+      "
+      rm -rf tmp
+      mkdir tmp
+      mkdir tmp/remote
+      mkdir tmp/repository
+      "
+    end
+
+    bash(from: 'tmp/remote', silent: true) do
+      "
+      git init --bare
+      "
+    end
+
+    bash(silent: true) do
+      "
+      git init
+      git remote add origin ../remote
+      touch a.txt
+      git add -A
+      git commit -m 'add a.txt'
+      git push origin master
+      git checkout -b develop
+      git push origin develop
+      git checkout -b staging
+      git push origin staging
+      git checkout -b uat
+      git push origin uat
+      git checkout develop
+      "
+    end
+
+  end
+
+  def teardown
+    `rm -rf ../tmp`
+  end
+
   def test_run_method_defined
     CreateNewFeatureBranch.new.run
   end
