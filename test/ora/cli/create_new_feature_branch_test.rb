116383dd30a436875bcedddea6a1880801b70ffe
Ducksan Cho <ducktyper@gmail.com>
2016-08-22 20:21:38 +1200

    Fix test

--- test/ora/cli/create_new_feature_branch_test.rb
+++ test/ora/cli/create_new_feature_branch_test.rb
@@ -10,6 +10,6 @@ class CreateNewFeatureBranchTest < Minitest::Test
 
   def test_run_method_defined
     CreateNewFeatureBranch.new.run(["new_feature"])
-    bash(from: REPOSITORY, "git status").include? "new_feature"
+    assert bash(from: REPOSITORY) {"git status"}.include? "new_feature"
   end
 end
