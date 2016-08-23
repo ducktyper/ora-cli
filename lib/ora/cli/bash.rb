23ef96129a29a0e15ab3549d8ed0a020904d419d
Ducksan Cho <ducktyper@gmail.com>
2016-08-23 21:24:05 +1200

    Change how silent works
    Sometimes we need output to grep some data but current silent can't do it.
    Let's just print outputs in test and silent setting will not print output
    in ruby.

--- lib/ora/cli/bash.rb
+++ lib/ora/cli/bash.rb
@@ -1,9 +1,10 @@
 module Bash
   def bash(from: nil, silent: false)
     yield.split("\n").map(&:strip).reject(&:empty?).map do |command|
-      move = "cd #{from} && " if from
-      out  = " &> /dev/null"  if silent
-      `#{move}#{command}#{out}`
+      move   = "cd #{from} && " if from
+      output = `#{move}#{command}`
+      puts output unless silent
+      output
     end.join("\n")
   end
 end
