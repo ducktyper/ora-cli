68209ac9420a7f3c7ea203a44b0cbcacd7b35a26
Ducksan Cho <ducktyper@gmail.com>
2016-08-23 21:53:20 +1200

    Use bash_helper in git_helper

--- test/support/git_helper.rb
+++ test/support/git_helper.rb
@@ -1,20 +1,17 @@
 module GitHelper
   include Bash
+  include BashHelper
 
   def commit_branch(branch_name, file_name)
-    bash(from: REPOSITORY, silent: true) do
-      "
+    bash_repo("
       git checkout #{branch_name}
       touch #{file_name}
       git add -A
       git commit -m 'add #{file_name}'
-      "
-    end
+    ")
   end
 
   def checkout(branch_name)
-    bash(from: REPOSITORY, silent: true) do
-      "git checkout #{branch_name}"
-    end
+    bash_repo("git checkout #{branch_name}")
   end
 end
