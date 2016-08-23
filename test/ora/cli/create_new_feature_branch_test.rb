0f1d9d277868042dfdb9fb9a917da1c676030df7
Ducksan Cho <ducktyper@gmail.com>
2016-08-23 22:18:18 +1200

    Extract method

--- test/ora/cli/create_new_feature_branch_test.rb
+++ test/ora/cli/create_new_feature_branch_test.rb
@@ -9,27 +9,31 @@ class CreateNewFeatureBranchTest < Minitest::Test
   end
 
   def test_run_method_defined
-    CreateNewFeatureBranch.new(REPOSITORY, silent: true).run(["new_feature"])
+    subject.run(["new_feature"])
     assert bash_repo("git status").include? "new_feature"
   end
 
   def test_start_from_develop_branch
     commit_branch(:develop, "develop.rb")
     checkout(:master)
-    CreateNewFeatureBranch.new(REPOSITORY, silent: true).run(["new_feature"])
+    subject.run(["new_feature"])
     assert bash_repo("ls").include? "develop.rb"
   end
 
   def test_pull_develop_branch
     commit_remote_branch(:develop, "develop.rb")
-    CreateNewFeatureBranch.new(REPOSITORY, silent: true).run(["new_feature"])
+    subject.run(["new_feature"])
     checkout(:develop)
     assert bash_repo("ls").include? "develop.rb"
   end
 
   def test_error_on_dirty_branch
     dirty_branch(:develop, "dirty.rb")
-    out = CreateNewFeatureBranch.new(REPOSITORY, silent: true).run(["new_feature"])
-    assert out.include? "ERROR"
+    assert subject.run(["new_feature"]).include? "ERROR"
+  end
+
+  private
+  def subject
+    CreateNewFeatureBranch.new(REPOSITORY, silent: true)
   end
 end
