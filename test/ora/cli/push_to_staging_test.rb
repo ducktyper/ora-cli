7b42586ffbc4906f9c7de13336c9404ffe202a27
Ducksan Cho <ducktyper@gmail.com>
2016-08-24 22:32:46 +1200

    Extract method

--- test/ora/cli/push_to_staging_test.rb
+++ test/ora/cli/push_to_staging_test.rb
@@ -3,23 +3,19 @@ require 'test_helper'
 class PushToStagingTest < Minitest::Test
   def setup
     reset_tmp
+    work_on_feature_branch
   end
   def teardown
     delete_tmp
   end
 
   def test_merge_to_staging
-    bash_repo("git checkout -b feature")
-    commit_branch(:feature, "test.txt")
     subject.run
     checkout(:staging)
     assert bash_repo("ls").include? "test.txt"
   end
 
   def test_push_to_staging
-    checkout(:staging)
-    bash_repo("git checkout -b feature")
-    commit_branch(:feature, "test.txt")
     subject.run
     checkout(:staging)
     assert bash_repo('git push origin staging').include? "up-to-date"
@@ -28,5 +24,10 @@ class PushToStagingTest < Minitest::Test
   private
   def subject
     PushToStaging.new(REPOSITORY, silent: true)
+  end
+
+  def work_on_feature_branch
+    bash_repo("git checkout -b feature")
+    commit_branch(:feature, "test.txt")
   end
 end
