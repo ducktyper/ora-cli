f36dc5b64a698ebc99dd9bd4309a4f7939c92e9e
Ducksan Cho <ducktyper@gmail.com>
2016-08-26 21:59:20 +1200

    Support print

--- lib/ora/cli/print.rb
+++ lib/ora/cli/print.rb
@@ -3,8 +3,15 @@ module Ora::Cli
     def puts_green text
       puts "\e[32m#{text}\e[0m"
     end
+    def print_green text
+      print "\e[32m#{text}\e[0m"
+    end
+
     def puts_red text
       puts "\e[31m#{text}\e[0m"
+    end
+    def print_red text
+      print "\e[31m#{text}\e[0m"
     end
   end
 end
