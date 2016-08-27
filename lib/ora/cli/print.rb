4a4f8b6984148f5185b98ff35b0bfc1c2514a408
Ducksan Cho <ducktyper@gmail.com>
2016-08-27 21:24:34 +1200

    Print returns itself

--- lib/ora/cli/print.rb
+++ lib/ora/cli/print.rb
@@ -6,18 +6,22 @@ module Ora::Cli
 
     def inline text
       print text unless @silent
+      text
     end
 
     def plain text
       puts text unless @silent
+      text
     end
 
     def green text
       puts "\e[32m#{text}\e[0m" unless @silent
+      text
     end
 
     def red text
       puts "\e[31m#{text}\e[0m" unless @silent
+      text
     end
   end
 end
