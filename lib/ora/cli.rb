b0fbadbdd5ee3eadb8fc58207e09b341aa3e8093
Ducksan Cho <ducktyper@gmail.com>
2016-08-27 22:06:46 +1200

    Refactor ora

--- lib/ora/cli.rb
+++ lib/ora/cli.rb
@@ -1,7 +1,10 @@
 module Ora
   module Cli
+    def self.run task, from
+      class_name = task.split('_').map(&:capitalize).join
+      Object.const_get("Ora::Cli::#{class_name}").new(from).run
+    end
   end
 end
 
 Dir[File.dirname(__FILE__) + '/cli/**/*.rb'].each {|file| require file }
-
