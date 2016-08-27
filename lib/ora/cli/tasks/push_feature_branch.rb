4c596a6e566149aac22cd648b7d264d32fe2de38
Ducksan Cho <ducktyper@gmail.com>
2016-08-27 14:12:59 +1200

    Update missing part

--- lib/ora/cli/tasks/push_feature_branch.rb
+++ lib/ora/cli/tasks/push_feature_branch.rb
@@ -2,17 +2,15 @@ require 'ora/cli/task'
 
 module Ora::Cli
   class PushFeatureBranch < Task
-    def run
-      bash(from: @from, silent: @silent) do
-        '
-        :clean_branch!
-        git checkout develop
-        git pull origin develop
-        git checkout #{branch}
-        git merge develop
-        git push origin #{branch}
-        '
-      end
+    def commands
+      '
+      :clean_branch!
+      git checkout develop
+      git pull origin develop
+      git checkout #{branch}
+      git merge develop
+      git push origin #{branch}
+      '
     end
   end
 end
