7888bae80a86f418ee1867e16f9375faa4a2f223
Ducksan Cho <ducktyper@gmail.com>
2016-09-18 20:56:40 +1200

    k	modified:   lib/ora/cli.rb

--- lib/ora/cli/task.rb
+++ lib/ora/cli/task.rb
@@ -24,6 +24,7 @@ module Ora::Cli
     end
 
     def continue(info)
+      @stdin.green("Continue task - #{info['task']}")
       variables = info['variables']
       @bash.run info['commands']
       save_on_fail
